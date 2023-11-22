1374a601883a693a5a4f910419703a5e8ca1df6f
Marko <markoni521@gmail.com>
2023-11-22 22:23:10 +0100

    Clean

--- src/state.ts
+++ src/state.ts
@@ -5,11 +5,6 @@ export type Position = {
 
 // List of positions for each queen
 export type State = [Position, Position, Position, Position, Position, Position, Position, Position];
-// export type Queens = [Position, Position, Position, Position, Position, Position, Position, Position];
-
-// export class State {
-//   constructor(public readonly queens: Queens) {}
-// }
 
 export type Move = {
   queenIndex: number;
